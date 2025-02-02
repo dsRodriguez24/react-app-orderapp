a1edd972b1c8144bbba4d2b630e94895f7606e16
Santiago <santiago.salasdezoom@gmail.com>
2025-02-02 00:55:11 -0500

    Modificar las rutas y el redireccionamiento de las rutas privadas

--- src/orderapp/router/RouterOrderApp.tsx
+++ src/orderapp/router/RouterOrderApp.tsx
@@ -1,7 +1,8 @@
-import { Route, Routes} from "react-router-dom"
-import { NavBar } from "../components/NavBar";
+import { Navigate, Route, Routes} from "react-router-dom"
+import { NavBar } from "../components/product-components/NavBar";
 import { Dashboard } from "../views/Dashboard";
 import { Products } from "../views/Products";
+import { Orders } from "../views/Orders";
 
 
 export const RouterOrderApp = () => {
@@ -12,8 +13,10 @@ export const RouterOrderApp = () => {
     <>
         <NavBar/>
         <Routes>
-            <Route path="dashboard" element={ <Dashboard/> }/>
+            {/* <Route path="dashboard" element={ <Dashboard/> }/> */}
             <Route path="productos" element={ <Products/> }/>
+            <Route path="ordenes" element={ <Orders/> }/>
+            <Route path="/*" element={ <Navigate to="productos"/> }/>
             
         </Routes>
     </>
